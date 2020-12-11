8733fc3d63de69b40ccb27b75e871c148c63a137
Youngsoo, Na <69225568+newpullin@users.noreply.github.com>
2020-12-11 16:36:28 +0900

    MainNav와 Footer 생성

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,42 @@
+const FileManagerPlugin = require('filemanager-webpack-plugin-fixed');
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+
+  outputDir: 'dist',
+  publicPath: '/',
+  assetsDir: 'static',
+
+  pages: {
+
+    home: {
+      template: 'public/index.html',
+      entry: 'src/pages/main_home.js',
+      filename: 'home.html',
+      title: 'SANTOKI - Simple IOT Service',
+      minify: false,
+    },
+  },
+  configureWebpack: {
+    plugins:[
+      new FileManagerPlugin({
+        onStart:{
+          delete: [
+            '../backend/static/**/',
+            '../backend/templates/**/',
+          ],
+        },
+        onEnd: {
+          copy: [
+            { source: 'dist/static', destination: '../backend/static/'},
+            { source: 'dist/favicon.ico', destination: '../backend/static/img/'},
+            { source: 'dist/home.html', destination: '../backend/templates/'},
+            //{ source: 'dist/post*.html', destination: '../backend/templates/notice/'},
+          ]
+        }
+      })
+    ]
+  }
 }
