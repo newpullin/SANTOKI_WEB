1cb49bd72c65bd0440189deb38178d77f5cdd0e6
Youngsoo, Na <69225568+newpullin@users.noreply.github.com>
2020-12-18 10:31:20 +0900

    정적 이미지로 교체

--- frontend/src/components/Carrot.vue
+++ frontend/src/components/Carrot.vue
@@ -2,23 +2,23 @@
   <v-container fluid>
     <v-layout align-center justify-center class="mt-8">
       <v-img
-        lazy-src="https://picsum.photos/10/15"
+        :src="require('../assets/carrot.jpg')"
+        class="my-3"
+        max-width="400px"
         max-height="300px"
-        max-width="500px"
-        src="https://picsum.photos/500/300"
       ></v-img>
     </v-layout>
     <v-card-text class="mt-3 text-center">
       <br /><br />
       <h1>Carrot</h1>
       <br /><br />
-      산토끼 전용 컨트롤러입니다.<br> PWA 기술로 만들어져 크로스플랫폼을
-      지원합니다.<br />
+      산토끼 전용 컨트롤러입니다.<br />
+      PWA 기술로 만들어져 크로스플랫폼을 지원합니다.<br />
       다운로드 없이 웹에서 사용가능합니다.<br />
-      물론 다운로드도 가능합니다. <br /><br />
+      다운로드도 가능합니다. <br /><br />
     </v-card-text>
     <v-layout align-center justify-center class="mt-9">
-      <v-btn text class="ma-2" href="https://shopping.naver.com/">
+      <v-btn text class="my-4" href="https://shopping.naver.com/">
         <v-icon large class="ma-1" color="indigo"> mdi-download </v-icon>
         사용하기
       </v-btn>
